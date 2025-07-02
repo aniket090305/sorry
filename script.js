fa9372e5de15a17e7016631e894ac44e75003e34
aniket090305 <aniketvishwakarma574@gmail.com>
2025-07-03 02:27:10 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function showLove() {
+  const response = document.getElementById("response");
+  response.innerHTML = "Thank you for forgiving me, Nikki ❤️";
+}
+window.onload = () => {
+  document.getElementById("typewriter").innerText = `Kam baat karne ka socha tha...  
+Par har pal mein tu hi yaad aayi... 💭  
+Mujhse galti ho gayi Nikki,  
+Teri khamoshi ne sab kuch samjhaayi... 😔`;
+};
