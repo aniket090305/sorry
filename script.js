d414f16aefdc66ee545d715b1c9674d8a06d3703
aniket090305 <aniketvishwakarma574@gmail.com>
2025-07-03 02:44:21 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 function showLove() {
   document.getElementById("response").innerText = "Thank you for forgiving me Nikki ❤️";
 }
+window.onload = () => {
+  document.getElementById("typewriter").innerText = `Kam baat karne ka socha tha...  
+Par har pal mein tu hi yaad aayi... 💭  
+Mujhse galti ho gayi Nikki,  
+Teri khamoshi ne sab kuch samjhaayi... 😔`;
+};
+
